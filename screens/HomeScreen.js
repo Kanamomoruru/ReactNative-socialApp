53b1874f6c0c306570c76d8a0723eb6af2579e56
Kanata Shimomoto <thunderpooh3@gmail.com>
2020-04-09 16:52:27 +0900

    add moment and Feed section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import { Ionicons } from "@expo/vector-icons"
+import moment from "moment"
 
-import { Ionicon } from "@expo/vector-icons"
+// temporary data until we pull from Firebase
+posts = [
+  {
+    id: '1',
+    name: 'Joe McKay',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    timestamp: 1569109273726,
+    avatar: require('../assets/avatar.jpg'),
+    image: require('../assets/tempImage1.jpeg')
+  },
+  {
+    id: '2',
+    name: 'Karyn Kim',
+    text:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    timestamp: 1569109273726,
+    avatar: require('../assets/avatar.jpg'),
+    image: require('../assets/tempImage2.jpeg')
+  },
+  {
+    id: '3',
+    name: 'Emerson Parsons',
+    text:
+      'Amet mattis vulputate enim nulla aliquet porttitor lacus luctus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant.',
+    timestamp: 1569109273726,
+    avatar: require('../assets/avatar.jpg'),
+    image: require('../assets/tempImage3.jpeg')
+  },
+  {
+    id: '4',
+    name: 'Kathie Malone',
+    text:
+      'At varius vel pharetra vel turpis nunc eget lorem. Lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor. Adipiscing tristique risus nec feugiat in fermentum.',
+    timestamp: 1569109273726,
+    avatar: require('../assets/avatar.jpg'),
+    image: require('../assets/tempImage4.jpeg')
+  }
+];
 
 export default class HomeScreen extends React.Component {
   renderPost = post => {
     return (
-      <View>
-        <Text>I am a post</Text>
+      <View style={styles.feedItem}>
+        <Image source={post.avatar} style={styles.avatar}></Image>
+        <View style={{ flex: 1 }}>
+          <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+            <View>
+              <Text style={styles.name}>{post.name}</Text>
+              <Text style={styles.timestamp}>{moment(post.timestamp).fromNow()}</Text>
+            </View>
+
+            <Ionicons name='ios-more' size={24} color="#73788b" />
+          </View>
+
+          <Text style={styles.post}>{post.text}</Text>
+
+          <Image source={post.image} style={styles.postImage} resizeMode="cover" />
+
+          <View style={{ flexDirection: "row"}}>
+            <Ionicons name='ios-heart-empty' size={24} color="#73788b" style={{ marginRight: 16 }} />
+            <Ionicons name='ios-chatboxes' size={24} color="#73788b" />
+          </View>
+        </View>
       </View>
     )
   }
@@ -17,6 +76,14 @@ export default class HomeScreen extends React.Component {
             <View style={styles.header}>
               <Text style={styles.headerTitle}>Feed</Text>
             </View>
+
+            <FlatList
+              style={styles.feed}
+              data={posts}
+              renderItem={({item}) => this.renderPost(item)}
+              keyExtractor={item => item.id}
+              showsVerticalScrollIndicator={false}
+            />
           </View>
         );
   }
@@ -40,5 +107,46 @@ const styles = StyleSheet.create({
     shadowRadius: 15,
     shadowOpacity: 0.2,
     zIndex: 10
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "500"
+  },
+  feed: {
+    marginHorizontal: 16
+  },
+  feedItem: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: "row",
+    marginVertical: 8
+  },
+  avatar: {
+    width: 36,
+    height: 36,
+    borderRadius: 18,
+    marginRight: 16
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#454D65"
+  },
+  timestamp: {
+    fontSize: 11,
+    color: "#C4C6CE",
+    marginTop: 4
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: "#838899"
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16
   }
 });
